5deec76ba2d2d24ba312cb10d7092e3ba7ad0e75
prakashtaz0091 <code.ing2468@gmail.com>
2024-08-25 21:31:36 +0545

    sign up

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,7 @@ from .import views
 urlpatterns = [
 
     # auth
+    path('accounts/signup/', views.signup_view, name="signup_page"),
     path('accounts/login/', views.login_view, name="login_page"),
     path('accounts/logout/', views.logout_view, name="logout_page"),
 
--- main/views.py
+++ main/views.py
@@ -1,11 +1,54 @@
 from django.shortcuts import render, redirect
-
-
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 def home(request):
     return render(request, 'main/home.html')
 
 
+
+def signup_view(request):
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password_confirm = request.POST.get('password_confirm')
+        # # signup_form = UserCreationForm(username=username, password1=password, password2 = password_confirm)
+        # signup_form = UserCreationForm(request.POST)
+
+        # if signup_form.is_valid():
+        #     print("valid")
+        if len(username) <6 and '@' not in username:
+            return render(request, 'main/signup.html', {'error':"Username must be at least 6 chars long or it must contain @"})
+        elif password != password_confirm:
+            return render(request, 'main/signup.html', {'error':"Password  and Confirm Password must be same "})
+        elif len(password) <6 and len(password_confirm) <6:
+            return render(request, 'main/signup.html', {'error':"Password  must be at least 6 chars long "})
+        else:
+
+            first_name = request.POST.get('first_name', 'default_name')
+            last_name = request.POST.get('last_name', 'default_name')
+            profile_pic = request.FILES.get('profile_picture')
+
+            user = User(
+                username = username  
+            )
+            user.password = user.set_password(password)
+            user.first_name = first_name
+            user.last_name = last_name
+
+            if profile_pic:
+                print(profile_pic, "*******************")
+
+
+
+            return redirect('login_page')
+
+   
+
+
+    return render(request, 'main/signup.html')
 
 def login_view(request):
     return render(request, 'main/login.html')
